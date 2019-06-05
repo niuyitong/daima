3379c182748b3289d16cf01a8d61cf203539525e
niuyitong <913451387@qq.com>
2019-06-06 00:47:55 +0800

    更新代码

--- 字符函数和字符串函数的模拟实现/字符函数和字符串函数的模拟实现/源.c
+++ 字符函数和字符串函数的模拟实现/字符函数和字符串函数的模拟实现/源.c
@@ -80,3 +80,80 @@ int my_strcmp(const char* str1, const char* str2)
 	else
 		return -1;
 }
+
+
+//strncpy
+//当num>=sizeof(src)时，拷贝正确，并在dest字符串后面加入'\0';
+//当num<sizeof(src)时，只拷贝src前n - 1个字符串到dest，不会为dest字符串后面加入'\0';
+char* my_strncpy(char* dest, const char* src,int num)
+{
+	char* cp = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+		while (num&&(*cp++ = *src++) != '\0')
+		{
+			num--;
+		}
+		if (num)
+		{
+			while (--num)
+				*cp++ = '\0';
+		}
+	return dest;
+}
+int main()
+{
+	char arr1[10] = "abcdef";
+	char arr2[5] = "ABC";
+	my_strncpy(arr1, arr2, 4);
+	printf("%s\n", arr1);
+	system("pause");
+	return 0;
+}
+
+
+//strncat追加
+char* my_strncat(char* dest, const char* src, int num)
+{
+	char* str = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	while (*str != '\0')
+	{
+		str++;
+	}
+	while (num && (*str++ = *src++) != '\0')
+	{
+		num--;
+	}
+
+	return dest;
+}
+int main()
+{
+	char arr1[30] = "to be ";
+	char arr2[20] = "or not to be";
+	my_strncat(arr1, arr2, 4);
+	printf("%s\n", arr1);
+	system("pause");
+	return 0;
+}
+
+
+//strncmp
+int strncmp(char * s1, char * s2, size_t n)
+{
+	if (!n)//n为无符号整形变量;如果n为0,则返回0
+		return 0;
+	//在接下来的while函数中
+	//第一个循环条件：--n,如果比较到前n个字符则退出循环
+	//第二个循环条件：*s1,如果s1指向的字符串末尾退出循环
+	//第三个循环条件：*s1 == *s2,如果两字符比较不等则退出循环
+
+	while (--n && *s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return(*s1 - *s2);
+}
