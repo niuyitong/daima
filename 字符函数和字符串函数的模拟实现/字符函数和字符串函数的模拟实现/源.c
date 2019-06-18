0f5d3c8c1c8ce0f2c10c67d0c8c9dd940b2eee61
niuyitong <913451387@qq.com>
2019-06-18 23:28:47 +0800

    更新

--- 字符函数和字符串函数的模拟实现/字符函数和字符串函数的模拟实现/源.c
+++ 字符函数和字符串函数的模拟实现/字符函数和字符串函数的模拟实现/源.c
@@ -157,3 +157,77 @@ int strncmp(char * s1, char * s2, size_t n)
 	}
 	return(*s1 - *s2);
 }
+
+
+
+//模拟实现strstr找子字符串
+char *my_strstr(const char* str1, const char* str2)
+{
+	assert(str1);
+	assert(str2);
+	char *cp = (char*)str1;
+	char*substr = (char*)str2;
+	char* s1 = NULL;
+	if (*str2 == '\0')
+		return NULL;
+	while (*cp)
+	{
+		s1 = cp;
+		substr = str2;
+		while (*s1 && *substr && (*s1 == *substr))
+		{
+			s1++;
+			substr++;
+		}
+		if (*substr == '\0')
+			return cp;
+		cp++;
+	}
+}
+
+
+//模拟实现memcpy
+void * memcpy(void * dst, const void* src, size_t count)
+{
+	void* ret = dst;
+	assert(dst);
+	assert(src);
+	while (count--)
+	{
+		*(char *)dst = *(char *)src;
+		dst = (char *)dst + 1;
+		src = (char *)src + 1;
+	}
+	return(ret);
+}
+
+
+//模拟实验memmove
+void * memmove(void * dst, const void * src, size_t count)
+{
+	void * ret = dst;
+	if (dst <= src || (char *)dst >= ((char *)src + count))
+	{
+		while (count--)
+		{
+			*(char *)dst = *(char *)src;
+			dst = (char *)dst + 1;
+			src = (char *)src + 1;
+		}
+	}
+	else
+	{
+		dst = (char *)dst + count - 1;
+		src = (char *)src + count - 1;
+
+
+		while (count--)
+		{
+			*(char *)dst = *(char *)src;
+
+			dst = (char *)dst - 1;
+			src = (char *)src - 1;
+		}
+	}
+	return ret;
+}
