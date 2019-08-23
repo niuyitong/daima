6cc93712820d5c383e984d24e73e7cb050504fa6
niuyitong <913451387@qq.com>
2019-08-23 14:56:36 +0800

    更新排序问题

--- test_8_22/test_8_22/test.c
+++ test_8_22/test_8_22/test.c
@@ -103,50 +103,136 @@
 //		printf("%d ", a[i]);
 //	}
 //}
+//
+////快速排序
+//void qusort(int s[], int start, int end)
+//{
+//	int i = start;
+//	int j = end;
+//	s[0] = s[start];
+//	while (i < j)
+//	{
+//		while (i < j&&s[0] < s[j])
+//			j--;
+//		if (i < j)
+//		{
+//			s[i] = s[j];
+//			i++;
+//		}
+//		while (i < j&&s[i] <= s[0])
+//			i++;
+//		if (i < j)
+//		{
+//			s[j] = s[i];
+//			j--;
+//		}
+//	}
+//		s[i] = s[0];
+//		if (start < i)
+//			qusort(s, start, j - 1);
+//		if (i < end)
+//			qusort(s, j + 1, end);
+//	}
+//
+//int main()
+//{
+//	int a[11] = { 0 };
+//	int i = 0;
+//	printf("请输入十个数：\n");
+//	for (i = 1; i <= 10; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//		qusort(a, 1, 10);
+//		for (i = 1; i <= 10; i++)
+//		{
+//			printf("%d ", a[i]);
+//		}
+//		printf("\n");
+//}
+//
+////选择排序
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int t = 0;
+//	int a[11] = { 0 };
+//	printf("请输入十个数：\n");
+//	for (i = 1; i < 11; i++)
+//		scanf("%d", &a[i]);
+//	for (i = 1; i <= 9; i++)
+//	for (j = i + 1; j <= 10; j++)
+//	{
+//		if (a[i]>a[j])
+//		{
+//			t = a[i];
+//			a[i] = a[j];
+//			a[j] = t;
+//		}
+//	}
+//	for (i = 1; i <= 10; i++)
+//		printf("%d ", a[i]);
+//	printf("\n");
+//	return 0;
+//}
 
-//快速排序
-void qusort(int s[], int start, int end)
-{
-	int i = start;
-	int j = end;
-	s[0] = s[start];
-	while (i < j)
-	{
-		while (i < j&&s[0] < s[j])
-			j--;
-		if (i < j)
-		{
-			s[i] = s[j];
-			i++;
-		}
-		while (i < j&&s[i] <= s[0])
-			i++;
-		if (i < j)
-		{
-			s[j] = s[i];
-			j--;
-		}
-	}
-		s[i] = s[0];
-		if (start < i)
-			qusort(s, start, j - 1);
-		if (i < end)
-			qusort(s, j + 1, end);
-	}
+////归并排序
+//void merge(int r[], int s[], int x1, int x2, int x3)
+//{
+//	int i = 0;
+//	int j = 0;
+//	int k = 0;
+//	i = x1;
+//	j = x2 + 1;
+//	k = x1;
+//	while ((i <= x2) && (j<=x3))
+//	if (r[i] <= r[j])
+//	{
+//		s[k] = r[i];
+//		i++;
+//		k++;
+//	}
+//	else
+//	{
+//		s[k] = r[j];
+//		j++;
+//		k++;
+//	}
+//	while (i <= x2)
+//		s[k++] = r[i++];
+//	while (j <= x3)
+//		s[k++] = r[j++];
+//}
+//void merge_sort(int r[], int s[], int m, int n)
+//{
+//	int p = 0;
+//	int t[20] = { 0 };
+//	if (m == n)
+//		s[m] = r[m];
+//	else
+//	{
+//		p = (m + n) / 2;
+//		merge_sort(r, t, m, p);
+//		merge_sort(r, t, p + 1, n);
+//		merge(t, s, m, p, n);
+//	}
+//}
+//int main()
+//{
+//	int a[11] = { 0 };
+//	int i = 0;
+//	printf("请输入十个数：");
+//	for (i = 1; i <= 10; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	merge_sort(a, a, 1, 10);
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("%d ", a[i]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
 
-int main()
-{
-	int a[11] = { 0 };
-	int i = 0;
-	printf("请输入十个数：\n");
-	for (i = 1; i <= 10; i++)
-	{
-		scanf("%d", &a[i]);
-	}
-		qusort(a, 1, 10);
-		for (i = 1; i <= 10; i++)
-		{
-			printf("%d ", a[i]);
-		}
-		printf("\n");
-}
